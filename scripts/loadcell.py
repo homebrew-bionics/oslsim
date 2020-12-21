3eaa9d672832db91fef875af5192ef5615f1d830
imsenthur <imsenthur@gmail.com>
2020-12-22 00:04:51 +0530

    Removed left loadcell.

--- scripts/loadcell.py
+++ scripts/loadcell.py
@@ -5,28 +5,15 @@ from gazebo_msgs.msg import ContactsState
 
 class Loadcell(object):
     def __init__(self):
-        self.ll_pub = rospy.Publisher('/oslsim/footL/loadcell/fz', Float32, queue_size=10)
-        self.lr_pub = rospy.Publisher('/oslsim/footR/loadcell/fz', Float32, queue_size=10)
-
-    def publish_ll_fz(self, val=0.0):
-        self.ll_pub.publish(val)
+        self.lr_pub = rospy.Publisher('/oslsim/loadcell/fz', Float32, queue_size=10)
 
     def publish_lr_fz(self, val=0.0):
         self.lr_pub.publish(val)
 
 class LoadcellSub(object):
     def __init__(self):
-        rospy.Subscriber('/oslsim/footL/loadcell', ContactsState, self.loadcell_l_callback)
-        rospy.Subscriber('/oslsim/footR/loadcell', ContactsState, self.loadcell_r_callback)
+        rospy.Subscriber('/oslsim/loadcell', ContactsState, self.loadcell_r_callback)
         self.lc = Loadcell()
-
-    def loadcell_l_callback(self, data):
-        val = 0.0
-        if len(data.states)>0:
-            val = data.states[-1].wrenches[0].force.z
-            # rospy.loginfo(rospy.get_caller_id() + "%s", data.states[-1].wrenches[0].force.z)
-
-        self.lc.publish_ll_fz(val)
 
     def loadcell_r_callback(self, data):
         val = 0.0
@@ -37,7 +24,6 @@ class LoadcellSub(object):
 
     def go(self):
         rospy.spin()
-
 
 if __name__ == '__main__':
     rospy.init_node('loadcell', anonymous=True)
